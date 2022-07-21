d62942c24a2372fcc069f5f5df0eec0d76daeca8
Rico <cangcimeng>
2022-07-21 13:30:57 +0700

    setting binding config

--- app/src/main/java/com/rico/challenge4/MainActivity.kt
+++ app/src/main/java/com/rico/challenge4/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rico.challenge4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rico.challenge4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
