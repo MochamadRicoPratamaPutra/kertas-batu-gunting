c99945c094d67813c87706eb32dab674b96a0ae5
Rico <cangcimeng>
2022-08-07 13:50:11 +0700

    add splash screen base

---
+++ app/src/main/java/com/rico/challenge4/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.rico.challenge4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
