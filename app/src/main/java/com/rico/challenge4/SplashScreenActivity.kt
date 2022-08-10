4e818e2a25ec2f7b0b3e6ade2b734ca79798aa6f
Rico <cangcimeng>
2022-08-10 09:05:19 +0700

    prototype adding image from url using glide in SplashScreenActivity.kt

--- app/src/main/java/com/rico/challenge4/SplashScreenActivity.kt
+++ app/src/main/java/com/rico/challenge4/SplashScreenActivity.kt
@@ -5,13 +5,23 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.rico.challenge4.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    var binding: ActivitySplashScreenBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         supportActionBar?.hide()
+
+//        binding?.apply {
+//            Glide.with(this@SplashScreenActivity)
+//                .load("https://i.ibb.co/HC5ZPgD/splash-screen1.png").into(ivLogoText)
+//        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashScreenActivity, ChoosingEnemyActivity::class.java)
